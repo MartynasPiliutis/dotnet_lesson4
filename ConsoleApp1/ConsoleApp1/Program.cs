e2b48a01c13648b9aca7604fa6ad7a9684431228
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-20 18:24:58 +0200

    L4P1 example2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,6 +16,13 @@ namespace ConsoleApp1
                 Console.WriteLine("i reiksme: {0}", i);
             }
             Console.ReadLine();
+
+            //Example2 pereina simbolius (ASCII kodavimo reiksmes)
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.WriteLine("c reiksme: {0}", c);
+            }
+            Console.ReadLine();
         }
     }
 }
