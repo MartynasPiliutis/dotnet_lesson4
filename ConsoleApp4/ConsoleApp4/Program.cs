847b00883367c5e6d22e581e75a4912414ae2b75
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-20 20:51:35 +0200

    L4P4 task1 complete

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            //Task1
+            int[] skaiciuMasyvas = new int[5];
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.Write("Iveskite skaiciu: ");
+                skaiciuMasyvas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (var skaicius in skaiciuMasyvas)
+            {
+                Console.Write("{0} ", skaicius);
+            }
+            Console.ReadLine();
         }
     }
 }
