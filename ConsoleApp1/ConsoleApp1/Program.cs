5e2e79c43e91fb302f7c5fc7b25bb5ffeb535f90
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-20 18:13:25 +0200

    L5P1 example 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example1
+            for (int i = 10-1; i >= 0; i--)
+            {
+                Console.WriteLine("i reiksme: {0}", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
