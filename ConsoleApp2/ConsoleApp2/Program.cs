b295f1c083df014c33cca48f7bd2e6e3fb70840b
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-20 19:19:04 +0200

    L4P2 task2 complete

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -21,6 +21,17 @@ namespace ConsoleApp2
             Console.WriteLine("Skaiciu suma yra {0}", skaiciuSuma);
             Console.ReadLine();
 
+            //Task2 isveda lyginius skaicius
+            Console.Write("Iveskite bet koki skaiciu: ");
+            int betKoksSkaicius1 = Convert.ToInt32(Console.ReadLine());
+            for (int ciklas1 = 0; ciklas1 <= betKoksSkaicius1; ciklas1 += 2)
+            {
+                Console.Write(", {0}", ciklas1);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pabaiga");
+            Console.ReadLine();
+
         }
     }
 }
