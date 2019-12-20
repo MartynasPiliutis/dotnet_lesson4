71a6954188db61b43e1d5e2adc61e9f926d885d5
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-20 21:45:59 +0200

    L4P4 task4 complete

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] skaiciuMasyvas = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i <= skaiciuMasyvas.Length-1; i++)
+            {
+                int a = rnd.Next(1, 100);
+                skaiciuMasyvas[i] = a;
+            }
+
+            foreach (var item in skaiciuMasyvas)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+            int temp = 0;
+
+            for (int i = 0; i <= skaiciuMasyvas.Length - 1; i++)
+            {
+                for (int h = i; h <= skaiciuMasyvas.Length - 1; h++)
+                {
+
+                    if (skaiciuMasyvas[i] > skaiciuMasyvas[h])
+                    {
+                        temp = skaiciuMasyvas[h];
+                        skaiciuMasyvas[h] = skaiciuMasyvas[i];
+                        skaiciuMasyvas[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Didziausias skaicius yra {0}", skaiciuMasyvas[skaiciuMasyvas.Length - 1]);
+            Console.WriteLine("Maziausias skaicius yra {0}", skaiciuMasyvas[0]);
+            
+            foreach (var item in skaiciuMasyvas)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
